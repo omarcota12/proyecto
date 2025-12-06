5a2de2be43bd4ca0797525fbf62db1da7b2da8b4
Omarcota12 <omarcotamedina28@gmail.com>
2025-12-06 12:18:34 -0700

    usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,25 +1,16 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
-const Usuario = sequelize.define("Usuario", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Usuario = sequelize.define(
+  "Usuario",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nombre: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
   },
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
+  {
+    tableName: "usuarios", // <- nombre exacto de tu tabla en la DB
+    timestamps: true
   }
-});
-
-export default Usuario;
+);
